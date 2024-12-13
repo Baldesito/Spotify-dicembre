776e560a59eca4ff96f6b81c14107064ef05b721
OCCHIO NERO <112170802+Baldesito@users.noreply.github.com>
2024-12-13 16:18:23 +0100

    commit

--- src/data/Stores/store.jsx
+++ src/data/Stores/store.jsx
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk"; // Importazione corretta
-import rootReducer from "./reducers"; // Import del tuo rootReducer
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
